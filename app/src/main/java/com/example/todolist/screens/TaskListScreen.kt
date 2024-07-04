7d1f6426c1a3506d0ff999eb12705efd15282047
ISLAM EMON <emon78697@gmail.com>
2024-07-04 12:18:00 +0700

    swipe to delete added

--- app/src/main/java/com/example/todolist/screens/TaskListScreen.kt
+++ app/src/main/java/com/example/todolist/screens/TaskListScreen.kt
@@ -3,17 +3,41 @@ package com.example.todolist.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,7 +45,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +55,7 @@ import com.example.todolist.R
 import com.example.todolist.data.Priority
 import com.example.todolist.data.Task
 import com.example.todolist.viewmodel.TaskViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -106,62 +130,91 @@ fun TaskList(navController: NavController, tasks: List<Task>, taskViewModel: Tas
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskRow(task: Task, navController: NavController, taskViewModel: TaskViewModel) {
     val color = getColorBasedOnPriority(task.priority)
     var isChecked by remember { mutableStateOf(task.isCompleted) }
+    val dismissState = rememberDismissState()
+    val coroutineScope = rememberCoroutineScope()
 
-    // Set the maximum number of characters you want to display in the description
-    val maxDescriptionLength = 28
-    val displayedDescription = if (task.description.length > maxDescriptionLength) {
-        task.description.take(maxDescriptionLength) + "..."
-    } else {
-        task.description
-    }
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp)
-            .clickable { navController.navigate("task_view/${task.id}") },
-        colors = CardDefaults.cardColors(
-            containerColor = color
-        )
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column {
-                Text(
-                    text = task.title,
-                    color = Color.White,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    text = displayedDescription,
-                    color = Color.White,
-                    fontSize = 18.sp,
-                )
+    LaunchedEffect(dismissState.isDismissed(DismissDirection.EndToStart) || dismissState.isDismissed(DismissDirection.StartToEnd)) {
+        if (dismissState.isDismissed(DismissDirection.EndToStart) || dismissState.isDismissed(DismissDirection.StartToEnd)) {
+            coroutineScope.launch {
+                taskViewModel.deleteTask(task)
             }
-            Checkbox(
-                checked = isChecked,
-                onCheckedChange = {
-                    isChecked = it
-                    task.isCompleted = it
-                    taskViewModel.updateTask(task)
-                },
-                colors = CheckboxDefaults.colors(
-                    checkedColor = Color.White,
-                    uncheckedColor = Color.White,
-                    checkmarkColor = Color.Black
-                )
-            )
         }
     }
+
+    SwipeToDismiss(
+        state = dismissState,
+        directions = setOf(DismissDirection.EndToStart, DismissDirection.StartToEnd),
+        background = {
+            val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+            val color = when (dismissState.targetValue) {
+                DismissValue.Default -> Color.Transparent
+                DismissValue.DismissedToEnd, DismissValue.DismissedToStart -> Color.Red
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color)
+                    .padding(horizontal = 20.dp),
+                contentAlignment = if (direction == DismissDirection.EndToStart) Alignment.CenterEnd else Alignment.CenterStart
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete Icon",
+                    tint = Color.White
+                )
+            }
+        },
+        dismissContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+                    .clickable { navController.navigate("task_view/${task.id}") },
+                colors = CardDefaults.cardColors(
+                    containerColor = color
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column {
+                        Text(
+                            text = task.title,
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = task.description.take(28) + if (task.description.length > 28) "..." else "",
+                            color = Color.White,
+                            fontSize = 18.sp,
+                        )
+                    }
+                    Checkbox(
+                        checked = isChecked,
+                        onCheckedChange = {
+                            isChecked = it
+                            task.isCompleted = it
+                            taskViewModel.updateTask(task)
+                        },
+                        colors = CheckboxDefaults.colors(
+                            checkedColor = Color.White,
+                            uncheckedColor = Color.White,
+                            checkmarkColor = Color.Black
+                        )
+                    )
+                }
+            }
+        }
+    )
 }
 
 fun getColorBasedOnPriority(priority: Priority?): Color {
